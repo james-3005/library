5b0b02ea640b6dea525ed77387eeff72a38ba527
Huy-No1 <71492647+Huy-No1@users.noreply.github.com>
2021-08-07 18:10:03 +0700

    test Image

--- src/Components/Atoms/LogoCom/LogoComponent.js
+++ src/Components/Atoms/LogoCom/LogoComponent.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import styles from './logoComponent.module.scss'
-import { Link, useHistory } from 'react-router-dom'
-import { useTranslation } from 'react-i18next';
+import React from "react";
+import styles from "./logoComponent.module.scss";
+import { Link, useHistory } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 function LogoComponent() {
-    const history= useHistory();
-    const {t}= useTranslation();
+    const history = useHistory();
+    const { t } = useTranslation();
     return (
-        <div className={styles.logo} onClick={() =>history.push('/')}>
+        <div className={styles.logo} onClick={() => history.push("/")}>
             <div>
-                <img src="image/svg/logo.svg" alt="logo" />
+                <img src="/image/svg/logo.svg" alt="logo" />
             </div>
             <div className={styles.logoTitle}>
                 <h3>{t("library")}</h3>
@@ -17,8 +17,7 @@ function LogoComponent() {
                 <p>{t("libraryName")}</p>
             </div>
         </div>
-
-    )
+    );
 }
 
-export default LogoComponent
+export default LogoComponent;
--- src/Components/Template/Background3/Background3.js
+++ src/Components/Template/Background3/Background3.js
@@ -1,15 +1,27 @@
-import React from 'react'
-import styles from './Background3.module.scss'
+import React from "react";
+import styles from "./Background3.module.scss";
 
-function Background1({children}) {
+function Background1({ children }) {
     return (
         <div className={styles.component}>
-            <img className={styles.footer} alt="" src="image/svg/footer3.svg"/>
-            <img className={styles.footer_animal} alt="" src="image/svg/footer3-animal.svg"/>
+            <img
+                className={styles.footer}
+                alt=""
+                src="/image/svg/footer3.svg"
+            />
+            <img
+                className={styles.footer_animal}
+                alt=""
+                src="/image/svg/footer3-animal.svg"
+            />
             {/* <img className={styles.footer_cloud} alt="" src="image/svg/footer2-cloud.svg"/> */}
-            <img className={styles.footer_moon} alt="" src="image/svg/footer3-moon.svg"/>
+            <img
+                className={styles.footer_moon}
+                alt=""
+                src="/image/svg/footer3-moon.svg"
+            />
             {children}
         </div>
-    )
+    );
 }
-export default Background1
+export default Background1;
