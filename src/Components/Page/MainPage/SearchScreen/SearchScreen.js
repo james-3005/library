2b4475cf5ebd878dff715ac86b324a3e5b64ed8b
Huy-No1 <71492647+Huy-No1@users.noreply.github.com>
2021-08-10 08:03:49 +0700

    asd

--- src/Components/Page/MainPage/SearchScreen/SearchScreen.js
+++ src/Components/Page/MainPage/SearchScreen/SearchScreen.js
@@ -32,11 +32,12 @@ function SearchScreen() {
         let exptype = type1;
         if (type3 !== "All") exptype = type3;
         else if (type2 !== "All") exptype = type2;
-        if (exptype == "All") exptype = 1;
+        // if (exptype == "All") exptype = 1;
+
         axios
             .get(
                 `${api}book?author=${author}&publishing_year=${year}&translator=${translator}&code_ddc=${
-                    exptype == 1 ? "" : exptype - 1
+                    exptype == "All" ? "" : exptype - 1
                 }&country_id=${country === "All" ? "" : country}`
             )
             .then((res) => {
